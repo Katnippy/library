8066c31d24102c509b94717faff7d54d102c20b5
kat <kat@pop-os.localdomain>
2023-09-15 14:22:39 +0100

    Add functionality to store books in a library array which is printed to the page.

---
+++ script.js
@@ -0,0 +1,42 @@
+const library = [];
+
+const books = document.querySelector('.books');
+function clearLibraryOnPage() {
+  while (books.firstChild) {
+    books.removeChild(books.lastChild);
+  }
+}
+
+function printLibraryToPage() {
+    clearLibraryOnPage();
+
+    for (book of library) {
+      entry = document.createElement('div');
+      entry.textContent = `${book.title} by ${book.author}, ${book.pages} 
+      pages`;
+      books.appendChild(entry);
+    }
+  }
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary(title, author, pages) {
+  let book = new Book(title, author, pages);
+  library.push(book);
+
+  printLibraryToPage();
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  addBookToLibrary(title, author, pages);
+});
+
